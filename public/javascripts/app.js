cf99cb9385267ebc77cf91418cd3740414f3b0ab
Yogesh Ashok Powar <yogesh@orowealth.com>
2018-12-04 23:09:28 +0530

    controllers: separate out
    Signed-off-by: Yogesh Ashok Powar <yogesh@orowealth.com>

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('freebib', ['ngFileUpload'])
+angular.module('freebib', [])
 .controller('main', function($scope, $locale, $http) {
 	$scope.headers = [
 		"bib",
@@ -82,29 +82,4 @@ angular.module('freebib', ['ngFileUpload'])
             }
         });
     };
-})
-.controller('upload', function($scope, $locale, Upload) {
-    $scope.submitFile = function (csv_file) {
-        alert("inside submitFile");
-        var file = $scope.csv_file;
-        if (!file) {
-            alert("File Empty");
-            return;
-        }
-        Upload.upload({
-            url : '/upload',
-            data : {
-                file: file
-            }
-        }).then(function (response) {
-            var resData = response.data;
-            $scope.error = response.data.error;
-            if (resData.error_code === 0) {
-                alert("Data uploaded successfully");
-            } else {
-                alert("Failed to uploaded data");
-            }
-        });
-    };
 });
-
